727b33731789cb19db99f494b5b63df632158ade
Megha7500 <142832150+Megha7500@users.noreply.github.com>
2024-02-17 11:10:15 +0530

    first commit

---
+++ src/test/java/com/SpringbootJenkinsDocker/SpringbootJenkinsDockerSample/SpringbootJenkinsDockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringbootJenkinsDocker.SpringbootJenkinsDockerSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
